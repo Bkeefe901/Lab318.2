25eec2cff7a151acbdf51fb941f8dafae3a2bda4
Bkeefe901 <bradyjokeefe@gmail.com>
2025-09-03 15:05:34 -0400

    First Commit

---
+++ server.mjs
@@ -0,0 +1,27 @@
+// Imports
+import express from 'express';
+
+
+// Setups
+const app = express();
+const PORT = 3000;
+
+
+// Middleware
+
+
+
+
+// Routes
+
+
+
+// Global Error Handling Middleware
+
+
+
+// Server Listener
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port: ${PORT}`);
+});
